4c2d076fa3fa96fac221f235b5210e20aad18000
RaphaelMW <raphael@mundowap.com.br>
2023-03-27 18:49:17 -0300

    test: ip validation

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_STATE ? "http://localhost:3333" : "/api/";
-const baseURLAccount = import.meta.env.VITE_API_VITE_API_ACCOUNTAPI;
+const baseURLAccount = import.meta.env.VITE_API_ACCOUNTAPI || "http://52.70.217.141/api/";
+
+console.log(import.meta.env.VITE_API_ACCOUNTAPI);
 
 export const api = axios.create({
   baseURL,
