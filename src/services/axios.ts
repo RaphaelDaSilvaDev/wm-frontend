616a122fda4dd97667963787b5fdc942a5e86ede
RaphaelMW <raphael@mundowap.com.br>
2023-03-28 13:48:06 -0300

    fix: alter domain wm manager

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_APISTATE ? "http://localhost:3333" : "/api/";
-const baseURLAccount = "http://52.70.217.141/api";
-//const baseURLAccount = "localhost:3332";
+const baseURLAccount = "https://admin.wm.app.br/api/";
+//const baseURLAccount = "http://localhost:3332";
 
 export const api = axios.create({
   baseURL,
