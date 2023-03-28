d543842b9734422c540f953432fc1c20137f1fca
RaphaelMW <raphael@mundowap.com.br>
2023-03-28 10:34:50 -0300

    fix: server

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_APISTATE ? "http://localhost:3333" : "/api/";
 const baseURLAccount = import.meta.env.VITE_APIACCOUNTAPI
   ? "http://localhost:3332"
-  : "52.70.217.141/api";
+  : "http://52.70.217.141/api";
 
 export const api = axios.create({
   baseURL,
