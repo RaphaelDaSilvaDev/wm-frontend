8d3e06f3185af21356582e145a4783f9e89d5904
RaphaelMW <raphael@mundowap.com.br>
2023-03-28 10:38:40 -0300

    fix: service

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -73,7 +73,7 @@ export function SideBar() {
             </S.Options>
           </S.Content>
           <S.Version>
-            <span>v-0.3.5</span>
+            <span>v-0.3.8</span>
           </S.Version>
         </S.Container>
       )}
