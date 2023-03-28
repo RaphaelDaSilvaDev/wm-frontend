c34665d610c5b28b01c91624bee80e9141f0fe5b
RaphaelMW <raphael@mundowap.com.br>
2023-03-28 09:36:09 -0300

    fix: env production

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_APISTATE ? "http://localhost:3333" : "/api/";
 const baseURLAccount = import.meta.env.VITE_APIACCOUNTAPI;
 
-console.log(import.meta.env.VITE_API_ACCOUNTAPI);
+console.log(import.meta.env.VITE_APIACCOUNTAPI);
 
 export const api = axios.create({
   baseURL,
